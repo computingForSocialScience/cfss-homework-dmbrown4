ccdd6e4231a7db0716fddd2942dee04ae8c90061
Danielle Brown <daniellebrown@Danielle-Browns-MacBook-Pro.local>
2015-02-07 12:32:02 -0600

    finished part 1.2

--- Assignment5/fetchArtist.py
+++ Assignment5/fetchArtist.py
@@ -9,23 +9,23 @@ def fetchArtistId(name):
     url = 'https://api.spotify.com/v1/search?q=' + name + "&type=artist"
     req = requests.get(url)
 
+    data = req.json()
+
     #checking for bad return value
     if not req.ok:
-        print "error"
-        return -1
+        print "error : " + data['error']['message']
+        return "error : " + data['error']['message']
 
-    data = req.json() 	
+     	
     #in JSON, index to artists, then items, then 0 location in items array, 
     #then id in location 0.
 
     #use len() to determine length of the array 'items' to make sure that
     #the artist actually exists.
-
-
     if  len(data['artists']['items']) > 0 :
         return data['artists']['items'][0]['id']
     else:
-        return -1
+        return 'error: artist does not exist'
       
 
 
@@ -34,21 +34,34 @@ def fetchArtistInfo(artist_id):
 	`returns a dictionary including the keys 'followers', 'genres', 
     'id', 'name', and 'popularity'.
     """
+
+    url = 'https://api.spotify.com/v1/artists/' + artist_id
+    req = requests.get(url)
+
+    data = req.json() 
+
+    if not req.ok:
+        print "error : " + data['error']['message']
+        return {}
+
+    
+   
+
    #create a new dictionary
     artist_info_dict = {}
-
     #keys for the dictionary
-    #artist_info_dict['followers'] = #number of Spotify followers
-    #artist_info_dict['genres'] = #list of Spotify genres associated w/ artist
-    #artist_info_dict['id'] = #Spotify artist ID
-    #artist_info_dict['name'] = #artist name
-    #artist_info_dict['popularity'] = #Spotify's popularity-meter, an integer
+    artist_info_dict['followers'] = data['followers']['total'] #number of Spotify followers
+    artist_info_dict['genres'] = data['genres']#array of Spotify genres associated w/ artist
+    artist_info_dict['id'] = artist_id
+    artist_info_dict['name'] = data['name']
+    artist_info_dict['popularity'] = int(data['popularity']) #Spotify's popularity-meter, an integer
+
+    return artist_info_dict
 
 
-    pass
 
-print fetchArtistId('M83')
-
+artist_id = fetchArtistId('Beatles')
+print fetchArtistInfo(artist_id)
 
 
 
