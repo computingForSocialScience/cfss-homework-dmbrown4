608981d3fe796c987d8fd8c9cbbac06f7028eeb7
Danielle Brown <daniellebrown@Danielle-Browns-MacBook-Pro.local>
2015-02-07 13:13:49 -0600

    finished part 2.2

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -31,12 +31,31 @@ def fetchAlbumInfo(album_id):
     """Using the Spotify API, take an album ID 
     and return a dictionary with keys 'artist_id', 'album_id' 'name', 'year', popularity'
     """
-    pass
+    url = 'https://api.spotify.com/v1/albums/' + album_id
+    req = requests.get(url)
+
+    data = req.json() 
+
+    if not req.ok:
+        print "error : " + data['error']['message']
+        return {}
 
 
+   #create a new dictionary
+    album_info_dict = {}
+    #keys for the dictionary
+    album_info_dict['artist_id'] = data['artists'][0]['id']
+    album_info_dict['album_id'] = album_id
+    album_info_dict['name'] = data['name']
+    album_info_dict['year'] = data['release_date'][0:4]
+    album_info_dict['popularity'] = int(data['popularity']) #Spotify's popularity-meter, an integer
+
+    return album_info_dict
 
 
-artist_id = fetchArtist.fetchArtistId('Beatles')
-print fetchAlbumIds(artist_id)
+print fetchAlbumInfo(fetchAlbumIds(fetchArtist.fetchArtistId('Sia'))[0])
+
+#artist_id = fetchArtist.fetchArtistId('Beatles')
+#print fetchAlbumIds(artist_id)
 
 
